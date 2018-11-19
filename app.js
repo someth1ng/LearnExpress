99ec1ad0d1264845a1514aa452943726d8adcc12
LAM <trinhoanglam214>
2018-11-19 21:05:32 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const express   = require('express');
+const hbs       = require('hbs');
+const fs        = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// middleware
+// app.use((req, res, next) => {
+//     var now = new Date().toString();
+//     var log = `${ now }: ${ req.method } ${ req.url }`;
+
+//     console.log(log);
+//     fs.appendFile('server.log', log + '\n', (err) => {
+//         if (err) {
+//             console.log('Unable to append to server.log');
+//         }
+//     });
+//     next();
+// });
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+});
+
+// use public dir
+app.use(express.static(__dirname + '/public'));
+
+// hbs helper
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', txt => {
+    return txt.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        msg: 'Welcome to my Website!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        id: 404,
+        message: 'Page not found.'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server has started!');
+});
